de5dcbd70d7372e7f52dd14185259d94d0e4b579
Samrat <samratshekhar42@gmail.com>
2017-09-06 00:01:40 +0530

    add initial schema to graphql middleware

--- js/schema/schema.js
+++ js/schema/schema.js
@@ -1,15 +1,39 @@
-import graphql from 'graphql';
-const {
+import lodash from 'lodash';
+
+import {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-} = graphql;
+  GraphQLSchema,
+} from 'graphql';
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+];
 
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: 'UserType',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString }},
+      resolve(parentValue, args) {
+        return lodash.find(users, { id: args.id });
+      }
+    }
+  },
+});
+
+export const schema = new GraphQLSchema({
+  query: RootQuery,
 });
--- js/server.js
+++ js/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import expressGraphQL from 'express-graphql';
+import { schema } from './schema/schema';
 
 const app = express();
 app.use('/graphql', expressGraphQL({
   graphiql: true,
+  schema,
 }))
 
 app.listen(4000, () => {
